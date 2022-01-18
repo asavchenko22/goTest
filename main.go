0b3c65a5fb09bab0b9c14bacf722a080b689fe7f
bi <asavchenko22@gmail.com>
2022-01-18 12:59:35 +0200

    some examples of using the defer statement

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	// "os"
 )
 
+// func g() int {
+// 	fmt.Println("g()")
+// 	return 0
+// }
+// func f(int) {
+// 	fmt.Println("f()")
+// }
+
+func f(nums [6]int) {
+	fmt.Printf("%v\n", nums)
+}
+
 func main() {
-	path := "/home/andrey/password"
-	getFile(path)
+
+	nums := [...]int{0, 1, 2, 3, 4, 5}
+	nums[0] += 1
+	defer f(nums)
+
+	// defer f(g())
+	// fmt.Println("Привіт, світ!")
+
+	// GetFileContent()
+
+	// path := "/home/andrey/password"
+	// getFile(path)
 }
 
-func getFile(path string) {
-	f, _ := os.Open(path)
-	defer f.Close()
+// func GetFileContent() string {
+// 	file, _ := os.Open("/home/bee/Документи/test.txt")
+// 	defer file.Close()
+// 	data := make([]byte, 100)
+// 	file.Read(data)
+// 	dataString := string(data)
+// 	//fmt.Printf(dataString)
+// 	if dataString == "Hello Go!" {
+// 		return "Mayank"
+// 	} else {
+// 		return "Anshul"
+// 	}
+// }
 
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		fmt.Println(s.Text())
-	}
-}
+// func getFile(path string) {
+// 	f, _ := os.Open(path)
+// 	defer f.Close()
+
+// 	s := bufio.NewScanner(f)
+// 	for s.Scan() {
+// 		fmt.Println(s.Text())
+// 	}
+// }
